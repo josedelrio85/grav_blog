5dcf848375f8095f4f66abcab637d230a1d2e8f1
Oscar Seoane <oscarseoane@bysidecar.onmicrosoft.com>
2020-03-24 10:32:02 +0100

    Commented code on code.js

--- user/themes/mytheme/code/code.js
+++ user/themes/mytheme/code/code.js
@@ -1,19 +1,18 @@
 import styles from './styles.scss'
 
-document.addEventListener('DOMContentLoaded', (e) => {
+// document.addEventListener('DOMContentLoaded', (e) => {
+//   const forms = document.forms;
+//   for(let i=0; i < forms.length; i++){
+//     forms[i].addEventListener('submit', (e) => {
+//       console.log("set logic when form["+i+"] is submitted");
+//     });
+//   }
+// });
 
-  // const forms = document.forms;
-  // for(let i=0; i < forms.length; i++){
-  //   forms[i].addEventListener('submit', (e) => {
-  //     console.log("set logic when form["+i+"] is submitted");
-  //   });
-  // }
-});
 
-
-jQuery(document).ready(function(){
-  $("p").click(function(){
-    $(this).hide();
-    console.log("hiding p elements to test jQuery with webpack");
-  });
-});
+// jQuery(document).ready(function(){
+//   $("p").click(function(){
+//     $(this).hide();
+//     console.log("hiding p elements to test jQuery with webpack");
+//   });
+// });
