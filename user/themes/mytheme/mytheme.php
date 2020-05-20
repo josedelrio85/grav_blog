8dc1e31eb56dc748b9718c36dd9fbd41769e3222
Jose del Río <josedelrio85@gmail.com>
2020-05-20 18:23:04 +0200

    playing with php and grav

--- user/themes/mytheme/mytheme.php
+++ user/themes/mytheme/mytheme.php
@@ -2,7 +2,22 @@
 namespace Grav\Theme;
 
 use Grav\Common\Theme;
-
+use Grav\Common\Uri;
 class MyTheme extends Theme
 {
+
+  public static function getSubscribedEvents()
+  {
+    return [
+      'onThemeInitialized' => ['onThemeInitialized', 0]
+    ];
+  }
+
+  public function onThemeInitialized() {
+    $uri = new \Grav\Common\Uri();
+
+    $ip = $uri->ip();
+    // var_dump($ip);
+    // var_dump($uri->base());
+  }
 }
